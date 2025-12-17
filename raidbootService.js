4c6ee3c96152f173dbe547ce2bdfade9621037dd
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-12-17 16:50:50 +0000

    Update raidbootService.js

--- raidbootService.js
+++ raidbootService.js
@@ -1,43 +1,53 @@
 const plans = [
       { 
         raiders: 5,
-        price: 50
+        price: 50,
+        link: 'https://t.me/+q8kjChOhn7o2MWQ0'
        },
       {
          raiders: 6, 
-         price: 60
+         price: 60,
+         link: 'https://t.me/+BeHBSEn_xP4wMmY0'
         },
       { 
         raiders: 7, 
-        price: 70
+        price: 70,
+        link: 'https://t.me/+reuKzK1zyqMzMGJk'
       },
       { 
         raiders: 8, 
-        price: 80
+        price: 80,
+        link: 'https://t.me/+dolxP3Erna5hM2E0'
       },
       { 
         raiders: 9,
-        price: 90
+        price: 90,
+        link: 'https://t.me/+Xg1vK5pMaoAxMTNk'
       },
       { 
         raiders: 10, 
-        price: 100
+        price: 100,
+        link: 'https://t.me/+yZUBQzhSIVZkZGZk'
       },
       { 
         raiders: 12, 
-        price: 120
+        price: 120,
+        link: 'https://t.me/+VFPE8fcpcoVhYTY8'
       },
       { 
         raiders: 15, 
-        price: 150
+        price: 150,
+        link: 'https://t.me/+Wamvc_HfXjY2ZmRk'
       },
       { 
         raiders: 18, 
-        price: 180
+        price: 180,
+        link: 'https://t.me/+3H2gu7PSb2s1ZDQ0'
       },
       { 
         raiders: 20, 
-        price: 200
+        price: 200,
+        link: 'https://t.me/+f1WUWpqU4D5hNTc8'
       }
     ];
 
@@ -58,7 +68,7 @@ const plans = [
           style: "currency",
           currency: "USD"
         })}/wk</p>
-        <button onclick="window.location.href='https://t.me/+YourGroupLink'">Join Telegram</button>
+        <button onclick="window.location.href='${plan.link}'">Join Telegram</button>
       `;
 
       grid.appendChild(card);
